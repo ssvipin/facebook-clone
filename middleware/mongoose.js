7c4217033e707dfa9cd08c797a0918bf0c1db9c2
vipin.kumar@borderfree.io <vipin.kumar@borderfree.io>
2023-09-05 11:43:16 +0530

    added clustor

--- middleware/mongoose.js
+++ middleware/mongoose.js
@@ -4,7 +4,7 @@ const connectDb = (handler) => async (req, res) => {
   if (mongoose.connections[0].readyState) {
     return handler(req, res);
   }
-  await mongoose.connect("mongodb://localhost:27017/fb-clone");
+  await mongoose.connect("mongodb+srv://admin:<Vipin001@>@cluster0.gwaeq.mongodb.net/?retryWrites=true&w=majority");
   return handler(req, res);
 };
 
